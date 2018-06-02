494b6d72e6e12da16ba8215f3b25ec97761093a3
Hubert N <32624296+hcnimi@users.noreply.github.com>
2018-06-02 09:27:52 -0700

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -168,6 +168,20 @@ const shareFilePendingUser = (file, email, cb) => {
   });
 };
 
+const searchPath = (userId, folderId, cb) => {
+  const query = '(SELECT SF.folder_id, F.name FROM (SELECT folder_id FROM files WHERE user_id = ? AND id IN (SELECT folder_id FROM files WHERE user_id = ? AND id IN (SELECT folder_id FROM files WHERE user_id = ?  AND id = ? )) UNION (SELECT folder_id FROM files WHERE user_id = ? AND id IN (SELECT folder_id FROM files WHERE user_id = ?  AND id = ? )) UNION (SELECT folder_id FROM files WHERE user_id = ? AND id = ?)) AS SF LEFT JOIN files As F ON SF.folder_id = F.id)  UNION (SELECT id, name FROM files WHERE user_id = ? AND id = ? )';
+
+  const data = [userId, userId, userId, folderId, userId, userId, folderId, userId, folderId, userId, folderId];
+
+  db.connection.query(query, [userId], function(err, result, fields) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, result);
+    }
+  });
+};
+
 const verifyFilePermissions = (userId, cb) => {
   const query = 'SELECT created_by_user_id AS user_id, is_public FROM files WHERE id = ?';
 
